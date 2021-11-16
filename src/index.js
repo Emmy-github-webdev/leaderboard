2d27399310c3bcd692bcd158ca7d32c048944e33
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-11-16 21:26:49 +0100

    Add markup for homepage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+/* eslint-disable import/no-cycle */
+import './css/styles.css';
 import _ from 'lodash';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join([''], ' ');
 
   return element;
 }
