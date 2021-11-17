9a1ac6c62046cdc55d406fa54e5b94e9e4211a2c
Emmanuel Oga <ogahemmanuel75@gmail.com>
2021-11-17 20:14:31 +0100

    Add get leaderboard method using fetch

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-/* eslint-disable import/no-cycle */
 import './css/styles.css';
-import leaderboardItems from './getItems.js';
+import getLeaderboardItems from './getItems.js';
 
-const getLeaderboardItem = () => {
-  leaderboardItems.forEach((leaderboardItem) => {
-    const list = document.getElementById('leaderboard-list');
-    const listLi = document.createElement('tr');
-    listLi.innerHTML = `
-     <td>${leaderboardItem.name}:</td>
-     <td>${leaderboardItem.score}</td>
-    `;
+window.onload = async () => {
+  const refreshItem = document.getElementById('refresh');
+  getLeaderboardItems();
 
-    list.appendChild(listLi);
-  });
+  refreshItem.addEventListener('click', getLeaderboardItems);
 };
-
-getLeaderboardItem();
